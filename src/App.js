5679c231beef868667df1794ce74977400e8639d
jalds01 <50274797+jalds01@users.noreply.github.com>
2019-10-23 18:26:53 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/home.js";
 
 function App() {
   return (
