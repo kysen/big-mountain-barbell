586f61fce3f0e4ecd58311f1bccbfdfa8ad4d7e0
kysen <kysenj@gmail.com>
2019-11-06 16:55:00 -0700

    hi

--- src/components/pages.js/about.js
+++ src/components/pages.js/about.js
@@ -1,7 +1,17 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
 const About = () => {
-  return <div className="about-container">About</div>;
+  return (
+    <div className="about-container">
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=eKEgbMwg8BU"
+        playing
+        muted
+        controls={false}
+      />
+    </div>
+  );
 };
 
 export default About;
