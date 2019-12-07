fb4d082d466503ea5203a356f00c768f04777663
kysen <kysenj@gmail.com>
2019-12-06 22:42:20 -0700

    hitler

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,16 +3,18 @@ import Form from "../form/Form";
 
 const Modal = ({ open, close, header = "", body, actions }) => {
   return (
-    <div className="modal-root" style={open ? {} : { display: 'none'}} onClick={close}>
-			<div className="modal-paper">
-				<h1 className="modal-header">
-					{header}
-				</h1>
-				<div className="modal-body" onClick={e => e.stopPropagation()}>
-					<Form body={body} />
-				</div>
-				{actions && actions}
-			</div>
+    <div
+      className="modal-root"
+      style={open ? {} : { display: "none" }}
+      onClick={close}
+    >
+      <div className="modal-paper">
+        <h1 className="modal-header">{header}</h1>
+        <div className="modal-body" onClick={e => e.stopPropagation()}>
+          <Form body={body} />
+        </div>
+        {actions && actions}
+      </div>
     </div>
   );
 };
