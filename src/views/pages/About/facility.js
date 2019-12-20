da7de6897579f3fb718fe5629f10dfebbce6f375
jalds01 <50274797+jalds01@users.noreply.github.com>
2019-12-19 19:45:12 -0700

    formatted the about page and made the call to action in the memberships.  That needs to be styles

--- src/views/pages/About/facility.js
+++ src/views/pages/About/facility.js
@@ -21,6 +21,7 @@ const Facility = () => {
             (heavier dumbbells coming soon), Kettlebells from 5-80 pounds, Rogue
             Foam Plyo Boxes and more.*
           </p>
+          <br />
           <p>
             Cardio Equipment: Stepmill, 2 Treadmills, 5 Rowers, 3 Exercise
             Bikes.
@@ -28,6 +29,8 @@ const Facility = () => {
           <p>Home of ChiroMovement Muscle & Joint Clinic</p>
           <p>*Good Vibes are FREE*</p>
         </div>
+        <br />
+
         <div className="mission">
           <h1>Our Mission</h1>
           <p>
@@ -36,6 +39,8 @@ const Facility = () => {
             and empowered to work on their goals.
           </p>
         </div>
+        <br />
+
         <div className="info">
           <h1>About</h1>
           <p>
@@ -44,12 +49,14 @@ const Facility = () => {
             our power to provide you with the space, equipment and positive
             atmosphere you need to make the most of your time.
           </p>
+          <br />
           <p>
             Big Mountain Barbell started in 2014 as a small warehouse gym filled
             with a couple of squat racks, used barbells, an old treadmill, and a
             vision to create a positive, productive training environment for
             humans from all ages, backgrounds and skill levels.
           </p>
+          <br />
           <p>
             By 2017 growth had surpassed initial expectations, and our owners,
             Zac and Lynndsey, knew it was time to grow. In June of 2017, we
@@ -57,10 +64,13 @@ const Facility = () => {
             boasts 11,000 square feet of gym space outfitted with only the best
             equipment.
           </p>
+
+          <br />
           <p>
             We offer 24/7 access Gym Memberships while maintaining a tight-knit
             community with emphasis on positive vibes.
           </p>
+          <br />
           <p>
             If you're over the crowded, bro gym vibe and re looking for the best
             equipment and facility in the valley-- come train at BMB!
