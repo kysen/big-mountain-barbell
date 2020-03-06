7636e84988b46b26dfd3dc35a35cd490b0ca28c4
Kysen Jackman <kysenj@gmail.com>
2020-03-06 16:44:46 -0700

    fit-club tab first commit with 4 column section almost complete

--- src/views/pages/FitClub/fit-club.js
+++ src/views/pages/FitClub/fit-club.js
@@ -1,7 +1,12 @@
 import React from "react";
+import FourPrograms from "./FourPrograms/four-programs";
 
 const FitClub = () => {
-  return <div className="fit-club-container">hi</div>;
+  return (
+    <div className="fit-club-container">
+      <FourPrograms />
+    </div>
+  );
 };
 
 export default FitClub;
